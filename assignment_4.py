599e92494019aea110695eacde7011aef7e23158
sailepradh <sailepradh@gmail.com>
2017-06-19 07:58:49 +0200

    making funcition for furture testing

--- assignment_4.py
+++ assignment_4.py
@@ -1,48 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import argparse
 import math
 
-'''Exercise from the group
-
-Modified by Sailen P based on original codes by Tejabadam
-
-Status : Does the work outlined howver issues with the output as it doesnot output as input; something wrong with dictionary creation
-         Unit testing needs to be done 
-         Functional module needs to be done
-         Version 1 
- '''
 
 def meansquare(x):
     a = x[0]
     b = x[1]
     rms = math.sqrt((0.25*((a[0]-b[0])**2 + (a[1]-b[1])**2 +(a[2]-b[2])**2 + (a[3]-b[3])**2)))
-    return (round(rms,3))
+    return round(rms,3)
 
+
+def seqdict(filenames):
+    seqs = {}
+    for f in filenames:
+        with open(f) as input_file:
+            lines = input_file.readlines()
+            start = lines.pop(0).replace("\n", "")
+            sequence = ""
+            for line in lines:
+                if line[0] == ">":
+                    seqs[start] = sequence
+                    sequence = ""
+                    start = line.replace("\n", "")
+                else:
+                    sequence += line.replace("\n", "")
+                    sequence = sequence.replace("N", "")
+                if sequence != "":
+                    seqs[start] = sequence
+    return seqs
+
+''' Main code for running of the above functions
+The basic plan is to modifying and unit test the above functions to make them
+usable for future as well.
+
+'''
 
 parser = argparse.ArgumentParser(description="loading fasta files")
 parser.add_argument("file", nargs='+', help='filename')
 args = parser.parse_args()
-
-
-seqs = {}
-for f in args.file:
-    with open(f) as input_file:
-        lines = input_file.readlines()
-        start = lines.pop(0).replace("\n", "")
-        sequence = ""
-        for line in lines:
-            if line[0] == ">":
-                seqs[start] = sequence
-                sequence = ""
-                start = line.replace("\n", "")
-            else:
-                sequence += line.replace("\n", "")
-                sequence = sequence.replace("N", "")
-        if sequence != "":
-            seqs[start] = sequence
-
+seqs = seqdict(args.file)
 
 results = {}
-for [keys, seq] in seqs.items():
+for [keys , seq] in seqs.items():
     counter = []
     keys_new = ((keys.split(' '))[0]).split(">")[1]
     for c in 'ATGC':
@@ -59,8 +60,8 @@ for i in range(l):
     for j in range(l):
         test = [com[i],com[j]]
         res = meansquare(test)
-        print (res, end = "\t")
-    print ("")
+        print(res, end = "\t")
+    print("")
 
 
 
