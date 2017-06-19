3d718ac9d280a94a6b89e3227aa72bcbdaa5cd8d
sailepradh <sailepradh@gmail.com>
2017-06-19 10:47:13 +0200

    use as main function

--- assignment_4.py
+++ assignment_4.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import argparse
 import math
-
 
 def meansquare(x):
     a = x[0]
@@ -20,48 +18,47 @@ def seqdict(filenames):
             start = lines.pop(0).replace("\n", "")
             sequence = ""
             for line in lines:
+                line.strip()
                 if line[0] == ">":
                     seqs[start] = sequence
                     sequence = ""
-                    start = line.replace("\n", "")
                 else:
-                    sequence += line.replace("\n", "")
+                    sequence += line
                     sequence = sequence.replace("N", "")
                 if sequence != "":
                     seqs[start] = sequence
     return seqs
 
-''' Main code for running of the above functions
-The basic plan is to modifying and unit test the above functions to make them
-usable for future as well.
-
+''' Main code for running of the above functions.
+Ao whenver we need to fasta files to dictionary with sequence names and sequences we can easily call the above functions. The only thing we need now is
+to unit test the above functions such that it is robust for different applications.
 '''
 
-parser = argparse.ArgumentParser(description="loading fasta files")
-parser.add_argument("file", nargs='+', help='filename')
-args = parser.parse_args()
-seqs = seqdict(args.file)
 
-results = {}
-for [keys , seq] in seqs.items():
-    counter = []
-    keys_new = ((keys.split(' '))[0]).split(">")[1]
-    for c in 'ATGC':
-        freq = round((seq.count(c) / len(seq)),3)
-        counter.append(freq)
-    results[keys_new] = counter
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="loading fasta files")
+    parser.add_argument("file", nargs='+', help='filename')
+    args = parser.parse_args()
+    seqs = seqdict(args.file)
 
-k = list(results.keys())
-com= list(results.values())
-l = len(k)
+    results = {}
+    for [keys , seq] in seqs.items():
+        counter = []
+        keys_new = ((keys.split(' '))[0]).split(">")[1]
+        for c in 'ATGC':
+            freq = round((seq.count(c) / len(seq)),3)
+            counter.append(freq)
+            results[keys_new] = counter
 
-for i in range(l):
-    print (k[i],end ="\t")
-    for j in range(l):
-        test = [com[i],com[j]]
-        res = meansquare(test)
-        print(res, end = "\t")
-    print("")
+    k = list(results.keys())
+    com= list(results.values())
+    l = len(k)
 
-
-
+    for i in range(l):
+        print (k[i],end = '\t')
+        for j in range(l):
+            test = [com[i],com[j]]
+            res = meansquare(test)
+            print(res, end = '\t')
+        print("")
